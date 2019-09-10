369edec39234a96ebffda301fdf9550dd38b80f7
Martin Weston <martinweston90@gmail.com>
2019-09-10 16:52:43 +0100

    change default font size to 10

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -68,7 +68,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("8")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("10")>  _
         Public Property FontSize() As Integer
             Get
                 Return CType(Me("FontSize"),Integer)
